4cc4ec61f2e55fc9e21496bc5f974906370b8218
Jakub Cejchan <frodo23@protonmail.com>
2021-08-30 11:46:36 +0200

    update app for heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const { router } = require('./routes')
 const app = express()
 const spreadsheetHelper = require('./spreadsheet-helper')
 const spreadsheetService = require('./spreadsheet-service')
-const port = process.env.SERVER_PORT
+const port = process.env.PORT || 5051
 
 const injectService = async (req, res, next) => {
   req.spreadsheetService = spreadsheetService
@@ -12,8 +13,11 @@ const injectService = async (req, res, next) => {
   next()
 }
 
+app.use(express.static(path.join(__dirname, '../public/')))
+
 app.use('/api', injectService, router)
 
 app.listen(port, () => {
   console.log(`backend listening at http://localhost:${port}`)
+  console.log(`bundling from ${path.join(__dirname, '../public')}`)
 })
